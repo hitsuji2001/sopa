f539ceb091c8b5f69aa34138b5dc46536777de82
hitsuji2001 <hitsuji2001@gmail.com>
2022-01-19 21:57:28 +0700

    Delete SDL2_image from flags

--- build.sh
+++ build.sh
@@ -3,31 +3,23 @@
 CXX="${CXX:-gcc}"
 SRC_APP="src/app/*.c"
 SRC_IMAGE="src/image_process/embeded_png.c"
-DEBUG_FLAG="-Werror -Wall -Wextra -pedantic -ggdb -lm -lSDL2_image"
-RELEASE_FLAG="-Werror -Wall -Wextra -pedantic -lm -lSDL2_image -O3"
+DEBUG_FLAG="-Werror -Wall -Wextra -pedantic -ggdb -lm"
+RELEASE_FLAG="-Werror -Wall -Wextra -pedantic -lm -O3"
 LIBS="sdl2"
 PNG_OUT="png2c"
 OUT="sopa"
 
-set -xe
-
 process_image() {
-    if [[ "$1" == "--release" ]]
-    then
-        $CXX $SRC_IMAGE $RELEASE_FLAG -o $PNG_OUT
-    elif [[ "$1" == "--debug" ]]
-    then
-        $CXX $SRC_IMAGE $DEBUG_FLAG -o $PNG_OUT
-    else
-        return 1
-    fi
+    $CXX $SRC_IMAGE -o $PNG_OUT -lm
 
     if [[ $? -ne 0 ]]
     then
         echo "Can not parse image to c file"
         exit 1
     else
+        echo "[CMD] File created '$PNG_OUT'"
         $(./"$PNG_OUT" > "./header/digits.h")
+        echo "[$PNG_OUT] File created '/header/digits.h'"
         return 0
     fi
 }
@@ -35,6 +27,7 @@ process_image() {
 if [[ $# -lt 1 ]]
 then
     echo "Usage: $(basename "$0") [flag]"
+    echo "There are only 2 flags: '--release' and '--debug'"
     exit 0
 else
     if [[ "$1" == "--release" ]]
@@ -46,16 +39,16 @@ else
         process_image $1
         $CXX $SRC_APP $DEBUG_FLAG -o $OUT $(pkg-config --cflags --libs $LIBS)
     else
-        echo "There are only 2 flags: '--release' and '--debug'"
+        echo "[ERROR] There are only 2 flags: '--release' and '--debug'"
         exit 1
     fi
 fi
 
 if [[ $? -ne 0 ]]
 then
-    echo "Compile failed"
+    echo "[ERROR] Compile failed"
     exit 1
 else
-    echo "Compile succesfully"
+    echo "[CMD] Compile succesfully"
     exit 0
 fi
